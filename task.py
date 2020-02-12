f32357546f85f2746e587cc3d754de22ab64354d
Panuhin Nikita <79215987151@mail.ru>
2020-02-12 10:08:17 +0300

    Update

--- task.py
+++ task.py
@@ -1,35 +1,58 @@
 # from time import sleep
 import os
+import re
 import matplotlib.pyplot as plt
 
 
 def main():
     with open("sholohov/_phrases.txt", "r", encoding='utf-8') as file:
-        phrases = list(filter(lambda x: x, list(map(lambda x: x.strip(), file.readlines()))))
+        phrases = list(filter(lambda x: x, list(map(lambda x: x.strip().lower(), file.readlines()))))
 
+    texts_length = 0
     count = [0] * len(phrases)
     don = [0] * len(phrases)
 
     for filename in os.listdir("sholohov"):
-        if filename != "_phrases.txt":
-            with open("sholohov/" + filename, "r", encoding='utf-8') as file:
-                text = file.read().strip()
-            if filename == "don.txt":
-                for i in range(len(phrases)):
-                    if text.count(phrases[i]) != 0:
-                        count[i] += (len(text.split()) / len(phrases[i].split())) / text.count(phrases[i])
-            else:
-                for i in range(len(phrases)):
-                    if text.count(phrases[i]) != 0:
-                        don[i] += (len(text.split()) / len(phrases[i].split())) / text.count(phrases[i])
-            print(filename)
+        if filename == "_phrases.txt":
+            continue
 
-    for i in range(len(count)):
-        count[i] = round(count[i] / (len(phrases) - 2), 3)
+        with open("sholohov/" + filename, "r", encoding='utf-8') as file:
+            text = file.read().strip().lower()
+
+        texts_length += len(text.split())
+
+        if filename == "don.txt":
+            for i in range(len(phrases)):
+                if text.count(phrases[i]) != 0:
+                    count[i] += text.count(phrases[i])
+        else:
+            for i in range(len(phrases)):
+                if text.count(phrases[i]) != 0:
+                    don[i] += text.count(phrases[i])
+        print(filename)
+
+    for i in range(len(phrases)):
+        count[i] *= len(phrases[i]) / (texts_length) * 100
+
+    for i in range(len(phrases)):
+        don[i] *= len(phrases[i]) / (texts_length) * 100
+
+    delta = [abs(don[i] - count[i]) for i in range(len(phrases))]
+
+    nums = list(range(len(phrases)))
+    ans = [y for x, y in sorted(zip(delta, nums), reverse=True)]
 
     print(count)
     print()
     print(don)
+
+    for e in ans:
+        print(phrases[e], delta[e])
+
+    plt.scatter(nums, count, c="#0000ff")
+    plt.scatter(nums, don, c="#ff0000")
+    plt.scatter(nums, delta, c="#00ff00")
+    plt.show()
 
 
 if __name__ == "__main__":
